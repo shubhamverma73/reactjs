d3cd53ece4169cfe9df54bb33af0897176ffb139
Shubham <shubhamkrverma73@gmail.com>
2021-01-05 15:08:44 +0530

    some modification

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import React from 'react'; //For only to show html content, its mordern code
 import ReactDOM from 'react-dom'; //For use html dom object
 
 
-var myOwnVar = <h1>Hello World...</h1>;
+//var myOwnVar = <h1>Hello World...</h1>;
 
-//ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root')); //h1 tag is call JSX (Javascript XML or extension)
-ReactDOM.render(myOwnVar, document.getElementById('root')); //h1 tag is call JSX (Javascript XML or extension)
+ReactDOM.render(
+        [
+          <h1>Hello World!</h1>,
+          <p>My first code is woking</p>
+        ], document.getElementById('root')); //h1 tag is call JSX (Javascript XML or extension)
+//ReactDOM.render(myOwnVar, document.getElementById('root')); //h1 tag is call JSX (Javascript XML or extension)
