66512420d08fbb9275f955e52a4f14cc1443a0d6
Shubham <shubhamkrverma73@gmail.com>
2021-01-07 14:46:54 +0530

    some changes in box design

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function Card(props) {
 	return (
 			<>
+			<div className="column">
 				<div className="card">
 					<img src={props.imgsrc} alt="Avatar" style={{width:"100%"}} />
 					<div className="container">
@@ -10,8 +11,28 @@ function Card(props) {
 						<p>{props.position}</p> 
 					</div>
 				</div>
+			</div>
 			</>
 		);
 }
 
+
+const Cdata = [
+				{
+					imgsrc: "https://www.w3schools.com/howto/img_avatar.png",
+					cardname: "Shubham",
+					position: "Software Engineer"
+				},{
+					imgsrc: "https://www.w3schools.com/howto/img_avatar.png",
+					cardname: "Sonu",
+					position: "Web Developer"
+				},{
+					imgsrc: "https://www.w3schools.com/howto/img_avatar.png",
+					cardname: "Vishwash",
+					position: "PHP Application Developer"
+				}
+
+			];
+
 export default Card;
+export { Cdata };
--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import Card from './Card';
+import Card, {Cdata} from './Card';
 
 //ReactDOM.render(<App />,document.getElementById('root'));
 
 ReactDOM.render(
 		<>
-		<Card imgsrc="https://www.w3schools.com/howto/img_avatar.png" cardname="Shubham" position="Software Engineer" />
-		<Card imgsrc="https://www.w3schools.com/howto/img_avatar.png" cardname="Sonu" position="Web Developer" />
-		<Card imgsrc="https://www.w3schools.com/howto/img_avatar.png" cardname="Vishwash" position="PHP Application Developer" />
+            <div className="row">
+                <Card imgsrc={Cdata[0].imgsrc} cardname={Cdata[0].cardname} position={Cdata[0].position} />
+                <Card imgsrc="https://www.w3schools.com/howto/img_avatar.png" cardname="Sonu" position="Web Developer" />
+                <Card imgsrc="https://www.w3schools.com/howto/img_avatar.png" cardname="Vishwash" position="PHP Application Developer" />
+            </div>
 		</>,
 			document.getElementById('root')
 		);
