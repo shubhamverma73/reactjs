1a2138fd92b40faf04453351d2aaa67edae1c9e4
Shubham <shubhamkrverma73@gmail.com>
2021-01-09 00:16:50 +0530

    learn hook

--- src/Hookcall.jsx
+++ src/Hookcall.jsx
@@ -1,2 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+
+
+const Hookcall = () => {
+    let currTIme = new Date().toLocaleTimeString();
+
+    const [currentTIme, setTIme] = useState(currTIme); //useState is hook function that return current data and function
+
+    const ChangeTIme = () => {
+        currTIme = new Date().toLocaleTimeString();
+        setTIme(currTIme);
+    }
+
+    return (
+        <>
+            <div className="manageDivCountBoxTime">
+                <span id="timeCount">{currentTIme}</span><br/><br/>
+                <button className="buttonClass" onClick={ChangeTIme}>Click Here</button>
+            </div>
+        </>
+    );
+}
+
+const Runtime = () => {
+    let currTIme = new Date().toLocaleTimeString();
+
+    const [currentTIme, setTIme] = useState(currTIme);
+
+    const updateTIme = () => {
+        currTIme = new Date().toLocaleTimeString();
+        setTIme(currTIme);
+    };
+    setInterval(updateTIme, 1000);
+
+    return (
+        <>
+            <div className="manageDivAutoTime">
+                <span id="timeAutoCount">{currentTIme}</span>
+            </div>
+        </>
+    );
+}
+
+export default Hookcall;
+export { Runtime };
--- src/index.js
+++ src/index.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card, {Cdata} from './Card';
-
-/*function ncard(val) {
-    return (
-        <Card imgsrc={val.imgsrc} cardname={val.cardname} position={val.position} linkRedirect={val.redirect} />
-    );
-}*/
-
-var getVal = 1;
-const clickFunction = () => {
-    getVal++;
-    document.getElementById('increaseCount').innerHTML = getVal;
-}
+import Hookcall, { Runtime } from './Hookcall';
 
 ReactDOM.render(
     <>
-        <div className="row">
-            {Cdata.map((val) => {
-                return (
-                            <Card key={val.id} imgsrc={val.imgsrc} cardname={val.cardname} position={val.position} linkRedirect={val.redirect} />
-                        );
-            })}
-        </div>
-        <div className="manageDivCountBox">
-            <span id="increaseCount">1</span><br/>
-            <button className="buttonClass" onClick={clickFunction}>Click Here</button>
-        </div>
+        <Hookcall />
+        <Runtime />
     </>,
         document.getElementById('root')
-    );
+);
