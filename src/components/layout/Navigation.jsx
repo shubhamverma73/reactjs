cab8b999fc4f1214b633b3cdd0dde87db954c00d
Shubham <shubhamkrverma73@gmail.com>
2021-01-13 01:35:48 +0530

    navbar menu modification

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Link, NavLink} from 'react-router-dom';
-import * as ReactBoot from 'react-bootstrap';
+import {Link, NavLink } from 'react-router-dom';
+import {Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Navigation = () => {
 
@@ -8,26 +9,26 @@ const Navigation = () => {
     script.src = "../../../node_modules/bootstrap/dist/js/bootstrap.js";
     script.async = true;*/
 
-    return (
+    return (    
         <>
             <div>
-                <ReactBoot.Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+                <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
                     <Link className="navbar-brand" to="/">React</Link>
-                    <ReactBoot.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <ReactBoot.Navbar.Collapse id="responsive-navbar-nav">
-                        <ReactBoot.Nav className="mr-auto">
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
                         <NavLink className="nav-link" to="/">Home</NavLink>
                         <NavLink className="nav-link" to="/about">About</NavLink>
                         <NavLink className="nav-link" to="/contact">Contact</NavLink>
-                        <ReactBoot.NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavLink className="dropdown-item" to="/increment-value">                                    
-                                <ReactBoot.NavDropdown.Item href="/increment-value">Link</ReactBoot.NavDropdown.Item>
-                            </NavLink>
-                        </ReactBoot.NavDropdown>
-                        </ReactBoot.Nav>
-                    </ReactBoot.Navbar.Collapse>
+                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            <LinkContainer to="/increment-value">
+                                <NavDropdown.Item >Increment Value</NavDropdown.Item>    
+                            </LinkContainer>
+                        </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
                     <NavLink className="btn btn-dark" to="/users/add">Add User</NavLink>
-                </ReactBoot.Navbar>
+                </Navbar>
             </div>
         </>
     );
