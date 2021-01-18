cfb73b6f3782b0a8e57564714e76d82881a5368a
Shubham <shubhamkrverma73@gmail.com>
2021-01-18 12:48:16 +0530

    navigation changes

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -17,17 +17,17 @@ const Navigation = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
-                        <NavLink className="nav-link" to="/">Home</NavLink>
-                        <NavLink className="nav-link" to="/about">About</NavLink>
-                        <NavLink className="nav-link" to="/contact">Contact</NavLink>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <LinkContainer to="/increment-value">
+                        <NavLink className="nav-link" exact to="/">Home</NavLink>
+                        <NavLink className="nav-link" exact to="/about">About</NavLink>
+                        <NavLink className="nav-link" exact to="/contact">Contact</NavLink>
+                        <NavDropdown title="Dropdown" exact id="collasible-nav-dropdown">
+                            <LinkContainer exact to="/increment-value">
                                 <NavDropdown.Item >Increment Value</NavDropdown.Item>    
                             </LinkContainer>
                         </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
-                    <NavLink className="btn btn-dark" to="/users/add">Add User</NavLink>
+                    <NavLink className="btn btn-dark" exact to="/users/add">Add User</NavLink>
                 </Navbar>
             </div>
         </>
