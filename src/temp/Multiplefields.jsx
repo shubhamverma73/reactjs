0bbc7c0153f56ee829c6aa723893cfdf9f712592
Shubham <shubhamkrverma73@gmail.com>
2021-01-27 21:39:10 +0530

    add multiple fields done

--- src/temp/Multiplefields.jsx
+++ src/temp/Multiplefields.jsx
@@ -17,18 +17,10 @@ const UserInformation = (props) => {
         })
     }
 
-    const onClickRmove = () => {
-        append({
-            firstname: "",
-            lastname: "",
-            emailaddress: "",
-            state: ""
-        })
-    }
     useEffect(() => {
         onClickAdd();
-    }, []);
-    
+    }, []); 
+        
     return (
         <div className="card mb-4">
             <div className="card-header">User Detsails</div>
@@ -37,16 +29,16 @@ const UserInformation = (props) => {
                     fields.map((item, index) => (
                         <div className="form-row form-group" key={item.id}>
                             <div className="col">
-                            <input type="text" className="form-control" name={`users[${index}].firstname`} defaultValue={item.firstname} ref={register()} placeholder="Enter Your First name" />
+                            <input type="text" className="form-control firstName" name={`users[${index}].firstname`} defaultValue={item.firstname} ref={register()} placeholder="Enter Your First name" />
                             </div>
                             <div className="col">
-                                <input type="text" className="form-control" name={`users[${index}].lastname`}  defaultValue={item.lastname} ref={register()} placeholder="Enter Your Last name" />
+                                <input type="text" className="form-control lastName" name={`users[${index}].lastname`}  defaultValue={item.lastname} ref={register()} placeholder="Enter Your Last name" />
                             </div>
                             <div className="col">
-                                <input type="text" className="form-control" name={`users[${index}].emailaddress`}  defaultValue={item.emailaddress} ref={register()} placeholder="Enter Your E-mail Address" />
+                                <input type="text" className="form-control emailAddress" name={`users[${index}].emailaddress`}  defaultValue={item.emailaddress} ref={register()} placeholder="Enter Your E-mail Address" />
                             </div>
                             <div className="col">
-                                <select className="form-control" name={`users[${index}].state`}  defaultValue={item.state} ref={register()} id="state">
+                                <select className="form-control state" name={`users[${index}].state`}  defaultValue={item.state} ref={register()} id="state">
                                 <option value="">Select Your State</option>
                                 <option value="Jharkhand">Jharkhand</option>
                                 <option value="Bihar">Bihar</option>
@@ -56,17 +48,16 @@ const UserInformation = (props) => {
                                 </select>
                             </div>
                             <div className="col">
-                            <button type="button" className="btn btn-danger" onClick={() => remove(index)}> <i className="fas fa-user-minus"></i></button>
+                            {
+                                (index === 0) ? 
+                                <button type="button" className="btn btn-danger" disabled> <i className="fas fa-user-minus"></i></button>
+                                :
+                                <button type="button" className="btn btn-danger" onClick={() => remove(index)}> <i className="fas fa-user-minus"></i></button>
+                            }
                             </div>
                         </div>
                     ))
                 }
-                {/* <button type="button" className="btn btn-primary" onClick={() => append({
-                    firstname: "",
-                    lastname: "",
-                    emailaddress: "",
-                    state: ""
-                })}>Add User</button> */}
                 <button type="button" className="btn btn-primary" onClick={onClickAdd}><i className="fas fa-user-plus"></i> Add User</button>
             </div>
         </div>
@@ -76,7 +67,7 @@ const UserInformation = (props) => {
 
 const Multiplefields = () => {
 
-    const { register, handleSubmit, control } = useForm();
+    const { register, handleSubmit, control, errors } = useForm();
 
     const onSubmit = data => {
         console.log(data)
@@ -88,20 +79,20 @@ const Multiplefields = () => {
                 <div className="card-header">User Detsails</div>
                 <div className="card-body">
                     <div className="form-group">
-                        <label htmlFor="fullname">Full Name</label>
-                        <input type="text" className="form-control" name="fullname" id="fullname" ref={register} />
+                        <p className="form-error">{errors.fullname && 'Fullname are required'}
+                        <input type="text" className="form-control" name="fullname" id="fullname" ref={register({required: true})} placeholder="Firstname" /></p>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="email">Email address</label>
-                        <input type="email" className="form-control"name="email" id="email" ref={register} />
+                        <p className="form-error">{errors.email && 'Email are required'}</p>
+                        <input type="email" className="form-control"name="email" id="email" ref={register({required: true})} placeholder="Email" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="phone">Phone Number</label>
-                        <input type="text" className="form-control" name="phone" id="phone" ref={register} />
+                        <p className="form-error">{errors.phone && 'Phone number are required'}</p>
+                        <input type="text" className="form-control" name="phone" id="phone" ref={register({required: true})} placeholder="Phone number" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" id="password" ref={register} />
+                        <p className="form-error">{errors.password && 'Password are required'}</p>
+                        <input type="password" className="form-control" name="password" id="password" ref={register({required: true})} placeholder="Password" />
                     </div>
                 </div>
             </div>
